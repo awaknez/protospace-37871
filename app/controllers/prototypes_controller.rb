aa3f6dd919b61fae598eca7580bab5472888febb
awaknez <zenz022021@gmail.com>
2022-05-02 15:36:11 +0900

    修正箇所の修正その２〜タイトルから詳細画面への遷移を修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,7 +3,7 @@ class PrototypesController < ApplicationController
   before_action :move_to_index, only: :edit
 
   def index
-    @prototypes = Prototype.all
+    @prototypes = Prototype.includes(:user)
   end
 
   def new
